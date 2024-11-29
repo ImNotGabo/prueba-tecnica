38fe30b03d7e806c5af24da383b481c11fda26d3
Gabriel Rodriguez <132179691+ImNotGabo@users.noreply.github.com>
2024-11-28 21:49:27 -0400

    testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,37 @@
-import { useCatImage } from './hooks/useCatImage'
-import { useCatFact } from './hooks/useCatFact'
+import { useCatImage } from './hooks/useCatImage';
+import { useCatFact } from './hooks/useCatFact';
 
 function App() {
-  // const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstWord}?fontSize=50&fontColor=red&json=true`
-  const { fact, fetchRandomFact } = useCatFact()
-  const { imageUrl } = useCatImage({ fact })
+	// const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstWord}?fontSize=50&fontColor=red&json=true`
+	const { fact, fetchRandomFact } = useCatFact();
+	const { imageUrl } = useCatImage({ fact });
 
-  const handleClick = () => {
-    fetchRandomFact()
-  }
+	const handleClick = () => {
+		fetchRandomFact();
+	};
 
-  return (
-    <main
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-      }}
-    >
-      <h1>Cat App</h1>
-      <button onClick={handleClick}>Get new fact</button>
-      {fact && <p>{fact}</p>}
-      {imageUrl && (
-        <img
-          src={`${imageUrl}`}
-          alt={`Image extracted using the first three words for ${fact}`}
-        />
-      )}
-    </main>
-  )
+	return (
+		<main
+			style={{
+				display: 'flex',
+				flexDirection: 'column',
+				alignItems: 'center',
+				justifyContent: 'center',
+			}}
+		>
+			<h1>Cat App</h1>
+			<button onClick={handleClick}>Get new fact</button>
+			{fact && <p>{fact}</p>}
+			{imageUrl && (
+				<img
+					src={`${imageUrl}`}
+					alt={`Image extracted using the first three words for ${fact}`}
+				/>
+			)}
+		</main>
+	);
 }
 
-export default App
+export default App;
 
+// testing commits
